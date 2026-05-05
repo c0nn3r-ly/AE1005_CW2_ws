d918db2b79a3ec0308a995133b991bac5b78f989
c0nn3r-ly <1659238620@qq.com>
2026-05-05 16:33:14 +0800

    Finish preliminary task

--- Yao_LIANG_20718529.m
+++ Yao_LIANG_20718529.m
@@ -5,6 +5,17 @@
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
 % Insert answers here
+clc;clear all;close all;
+
+a = arduino("/dev/cu.usbserial-1310","Uno");
+
+%let led blink 5 times
+for i = 1:5
+    writeDigitalPin(a,"D2",1);
+    pause(0.5);
+    writeDigitalPin(a,"D2",0);
+    pause(0.5);
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
